23b30c2f00e9e7576e4ada4f01454b4c9dbe4ed6
TshireletsoMogwere <tmogwere09@gmail.com>
2024-05-07 20:30:24 +0200

    checked in input fiekds are empty and checked if division is not performed

--- scripts.js
+++ scripts.js
@@ -5,5 +5,11 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
+  if (dividend === "" || divider === "") {
+    throw Error("Division not performed. Both values are required in inputs.Try again");
+  }
+  if (divider === 0) {
+    throw Error("Division not performed. Invalid number provided. Try again");
+  }
   result.innerText = Math.floor(dividend / divider);
 });
