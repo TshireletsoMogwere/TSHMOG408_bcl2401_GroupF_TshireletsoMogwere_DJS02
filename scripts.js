281f3d33f90d2a47eb2c9af53359d795ac13e358
TshireletsoMogwere <tmogwere09@gmail.com>
2024-05-09 14:51:12 +0200

    modified conditional statement

--- scripts.js
+++ scripts.js
@@ -14,7 +14,7 @@ form.addEventListener("submit", (event) => {
     throw Error("Division not performed. Invalid number provided. Try again");
   }
   if (isNaN(dividend) || isNaN(divider)) {
-    result.classList.add("critical-error");
+    result.classList.add("critical-error", "error-message");
     throw Error("Something critical went wrong. Please reload the page");
   }
   result.innerText = Math.floor(dividend / divider);
